b70ceabf4858bc8266d0d7afeebce4f1a9ba95e3
Will Davidow <w.davidow@gmail.com>
2019-12-05 17:28:35 -0500

    cleanup; add in config for browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
     entry: './src/js/app.js',
@@ -16,4 +17,18 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new BrowserSyncPlugin({
+            notify: false,
+            host: 'localhost',
+            port: 3001, // this is the port you develop on. Can be anything
+            logLevel: 'silent',
+            files: [
+                './wp/wp-content/themes/%%%PACKAGE_NAME%%%/**/*.php',
+                './wp/wp-content/themes/%%%PACKAGE_NAME%%%/**/*.js',
+                './wp/wp-content/themes/%%%PACKAGE_NAME%%%/**/*.css',
+            ],
+            proxy: 'http://localhost:3000/', // This port must match docker-compose.yml
+          }),
+    ]
 };
